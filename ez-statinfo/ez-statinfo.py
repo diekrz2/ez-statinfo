57668029e0020c74f7e075cab277e90154b035ec
diekrz2 <162576315+diekrz2@users.noreply.github.com>
2025-10-14 12:41:26 +0200

    Update ez-statinfo.py
    Simple close button added

--- ez-statinfo/ez-statinfo.py
+++ ez-statinfo/ez-statinfo.py
@@ -9,16 +9,25 @@ def update_stats():
     disk = psutil.disk_usage('/')
     text = f"CPU: {cpu:.0f}%    RAM: {mem.used // 1024**2}MB / {mem.total // 1024**2}MB    SSD: {disk.used // 1024**3}GB / {disk.total // 1024**3}GB"
     label.config(text=text)
+    
 # Update every 1s
     root.after(1000, update_stats)  
 
+def close_app():
+    root.quit()
+
 root = tk.Tk()
-root.title("Easy System Info")
-root.geometry("600x50")
+root.geometry("600x80")
 root.configure(bg="black")
 
+# No title bar if True:
+root.overrideredirect(True)
+
 label = tk.Label(root, text="", fg="white", bg="black", font=("Arial", 11))
-label.pack(expand=True)
+label.pack(expand=True, fill='both')
+
+button = tk.Button(root, text="x", fg="white", bg="black", relief="solid", bd=2, width=1, command=close_app)
+button.pack(side='bottom')
 
 update_stats()  
 root.mainloop()
