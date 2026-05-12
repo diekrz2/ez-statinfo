4b11b81adf76bbe9880c2548b24f331be698cbe3
diekrz2 <162576315+diekrz2@users.noreply.github.com>
2026-05-12 12:07:40 +0200

    some code cleaning
    mostly whitespaces and long lines

--- ez-statinfo/ez-statinfo.py
+++ ez-statinfo/ez-statinfo.py
@@ -3,25 +3,33 @@
 import psutil
 import tkinter as tk
 
+
 def update_stats():
     cpu = psutil.cpu_percent(interval=0.1)
     mem = psutil.virtual_memory()
     disk = psutil.disk_usage('/')
-    
+
 # The space between statistics can be adjusted
-    text = f"CPU: {cpu:.0f}%     RAM: {mem.used // 1024**2}MB / {mem.total // 1024**2}MB     SSD: {disk.used // 1024**3}GB / {disk.total // 1024**3}GB"
+    text = (
+            f"CPU: {cpu:.0f}%     "
+            f"RAM: {mem.used // 1024**2}MB / {mem.total // 1024**2}MB     "
+            f"SSD: {disk.used // 1024**3}GB / {disk.total // 1024**3}GB"
+            )
     label.config(text=text)
-    
+
 # Update every 1s
-    root.after(1000, update_stats)  
+    root.after(1000, update_stats)
+
 
 def close_app():
     root.quit()
+
 
 # Drag functions
 def drag(event):
     root.drag_start_x = event.x_root
     root.drag_start_y = event.y_root
+
 
 def drag_move(event):
     dx = event.x_root - root.drag_start_x
@@ -32,6 +40,7 @@ def drag_move(event):
     root.drag_start_x = event.x_root
     root.drag_start_y = event.y_root
 
+
 # Appearance
 root = tk.Tk()
 root.geometry("600x80")
@@ -41,18 +50,20 @@ root.configure(bg="black")
 root.overrideredirect(True)
 
 # Button
-button = tk.Button(root, text="x", fg="white", bg="black", relief="solid", bd=2, width=1, command=close_app)
+button = tk.Button(root, text="x", fg="white", bg="black",
+                   relief="solid", bd=2, width=1, command=close_app)
 button.pack(side='bottom')
 
 # Drag Bar
-drag_bar = tk.Canvas(root, bg="black", highlightthickness=0, height=10, width=450)
-drag_bar.create_line(0, 5, 450, 5, fill="white")  
-drag_bar.pack(side='top', pady=1, anchor='n')  
+drag_bar = tk.Canvas(root, bg="black", highlightthickness=0,
+                     height=10, width=450)
+drag_bar.create_line(0, 5, 450, 5, fill="white")
+drag_bar.pack(side='top', pady=1, anchor='n')
 drag_bar.bind("<Button-1>", drag)
 drag_bar.bind("<B1-Motion>", drag_move)
 
 label = tk.Label(root, text="", fg="white", bg="black", font=("Noto Sans", 11))
 label.pack(expand=True, fill='both')
 
-update_stats()  
+update_stats()
 root.mainloop()
